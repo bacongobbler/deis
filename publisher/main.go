fa5f70cf7c9572068576afec822972dc488cccb2
Matthew Fisher <mfisher@engineyard.com>
2015-05-19 14:51:27 -0700

    ref(publisher): use Go standard idioms for flag init
    This same initialization technique is demonstrated in both confd and
    logger.

--- publisher/main.go
+++ publisher/main.go
@@ -14,53 +14,51 @@ import (
 	"github.com/deis/deis/publisher/server"
 )
 
-const (
-	defaultBindAddr    				 = "localhost:6060"
-	defaultRefreshTime time.Duration = 10 * time.Second
-	defaultEtcdTTL     time.Duration = defaultRefreshTime * 2
-	defaultHost                      = "127.0.0.1"
-	defaultDockerHost                = "unix:///var/run/docker.sock"
-	defaultEtcdHost                  = "127.0.0.1"
-	defaultEtcdPort                  = "4001"
-	defaultLogLevel                  = "error"
+var (
+	bindAddr        string
+	refreshDuration time.Duration
+	etcdTTL         time.Duration
+	host            string
+	dockerAddr      string
+	etcdAddr        string
+	logLevel        string
 )
 
-var (
-	bindAddr        = flag.String("bind-addr", defaultBindAddr, "The IP address and port to bind to")
-	refreshDuration = flag.Duration("refresh-duration", defaultRefreshTime, "The time to wait between etcd refreshes.")
-	etcdTTL         = flag.Duration("etcd-ttl", defaultEtcdTTL, "The TTL for all of the keys in etcd.")
-	host            = flag.String("host", defaultHost, "The host where the publisher is running.")
-	dockerHost      = flag.String("docker-host", defaultDockerHost, "The host where to find docker.")
-	etcdHost        = flag.String("etcd-host", defaultEtcdHost, "The etcd host.")
-	etcdPort        = flag.String("etcd-port", defaultEtcdPort, "The etcd port.")
-	logLevel        = flag.String("log-level", defaultLogLevel, "Acceptable values: error, debug")
-)
+func init() {
+	flag.StringVar(&bindAddr, "bind-addr", "localhost:6060", "The IP address and port to bind to")
+	flag.DurationVar(&refreshDuration, "refresh-duration", 10 * time.Second, "The time to wait between etcd refreshes")
+	flag.DurationVar(&etcdTTL, "etcd-ttl", refreshDuration * 2, "The TTL for all of the keys in etcd")
+	flag.StringVar(&host, "host", "127.0.0.1", "The host where the publisher is running")
+	flag.StringVar(&dockerAddr, "docker-addr", "unix:///var/run/docker.sock", "The bind address to the docker API")
+	flag.StringVar(&etcdAddr, "etcd-addr", "http://127.0.0.1:4001", "The bind address to the etcd host")
+	flag.StringVar(&logLevel, "log-level", "error", "Acceptable values: error, debug")
+}
 
 func main() {
 	flag.Parse()
 
 	log.Println("booting publisher...")
 
-	dockerClient, err := docker.NewClient(*dockerHost)
+	dockerClient, err := docker.NewClient(dockerAddr)
 	if err != nil {
 		log.Fatal(err)
 	}
-	etcdClient := etcd.NewClient([]string{"http://" + *etcdHost + ":" + *etcdPort})
+	etcdClient := etcd.NewClient([]string{etcdAddr})
 
-	server := server.New(dockerClient, etcdClient, *host, *logLevel)
+	server := server.New(dockerClient, etcdClient, host, logLevel)
 
-	go server.Listen(*etcdTTL)
+	go server.Listen(etcdTTL)
 
 	go func() {
 		msg := fmt.Sprintf("profiler listening on %s", bindAddr)
-		if err := http.ListenAndServe(*bindAddr, nil); err != nil {
+		if err := http.ListenAndServe(bindAddr, nil); err != nil {
 			msg = err.Error()
 		}
 		log.Println(msg)
 	}()
 
 	for {
-		go server.Poll(*etcdTTL)
-		time.Sleep(*refreshDuration)
+		go server.Poll(etcdTTL)
+		time.Sleep(refreshDuration)
 	}
 }
